75e4159c48220db2c38ab56c9bb02cc7a181e52e
Rick Wu <wurick666@gmail.com>
2021-12-17 17:07:23 +0800

    change while

--- password_retry.py
+++ password_retry.py
@@ -1,12 +1,10 @@
 ans = 'a123456' 
 i = 3 #密碼驗證次數
-while True:
+while i > 0:
 	password = input('請輸入密碼')
 	if password == ans:
 		print('恭喜登入成功')
 		break
 	else:
 		i = i - 1
-		print('密碼錯誤！', i , '次機會')
-		if i == 0:
-			break
+		print('密碼錯誤！剩餘', i ,'次機會')
